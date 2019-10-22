2c9d985b975c7c8cc0e800a9c4de754df0be5474
Farseer <1369221804@qq.com>
2019-10-22 16:59:39 +0800

    add event util

---
+++ src/utils/event.js
@@ -0,0 +1,23 @@
+const list = {}
+
+export function on(name, func) {
+  if (list[name]) {
+    list[name].push(func)
+  } else {
+    list[name] = [func]
+  }
+}
+
+export function off(name, func) {
+  if (!list.name) return
+  if (func) {
+    list[name] = list[name].filter(o => o !== func)
+  } else {
+    list[name] = []
+  }
+}
+
+export function emit(name, ...args) {
+  if (!list.name) return
+  list.name.forEach(o => o(...args))
+}
